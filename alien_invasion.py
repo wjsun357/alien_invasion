cd1a69bbc75e868930bf51c3cfecf99dd9e01e95
wjsunscut <31986538+wjsunscut@users.noreply.github.com>
2018-05-02 22:42:50 +0800

    2018.5.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,19 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
-    pygame.init()
+    pygame.init()   #初始化背景设置
     ai_settings=Settings()
-    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))    #设置屏幕
     pygame.display.set_caption("Alien Invasion")
-    ship=Ship(screen)
-    while True:
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+    ship=Ship(ai_settings,screen)
+    while True:     #控制时间循环以及管理屏幕更新
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ class Settings():
     def __init__(self):
         self.screen_width=800
         self.screen_height=400
-        self.bg_color=(230,230,230)
+        self.bg_color=(0,191,255)
+        self.ship_speed_factor=0.1
