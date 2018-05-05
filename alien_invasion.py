5c96c24e15f58a1681883a11cc592fd80cb8debe
wjsunscut <31986538+wjsunscut@users.noreply.github.com>
2018-05-05 21:41:40 +0800

    finish ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -10,10 +11,12 @@ def run_game():
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))    #设置屏幕
     pygame.display.set_caption("Alien Invasion")
     ship=Ship(ai_settings,screen)
+    bullets=Group()
     while True:     #控制时间循环以及管理屏幕更新
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -3,4 +3,9 @@ class Settings():
         self.screen_width=800
         self.screen_height=400
         self.bg_color=(0,191,255)
-        self.ship_speed_factor=0.1
+        self.ship_speed_factor=0.2
+        self.bullet_speed_factor=0.2
+        self.bullet_width=3
+        self.bullet_height=15
+        self.bullet_color=(60,60,60)
+        self.bullets_allowed=3
